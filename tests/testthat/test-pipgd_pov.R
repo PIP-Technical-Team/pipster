f5d31d2581f6da7ef5c5864c0cf034dffa4b6f25
zander-prinsloo <prinsloo.zander@gmail.com>
2024-01-02 07:46:25 -0500

    start severity test

--- tests/testthat/test-pipgd_pov.R
+++ tests/testthat/test-pipgd_pov.R
@@ -28,12 +28,64 @@ weight_cumshare  <- pip_gd$P
 #_______________________________________________________________________________
 
 
-
-
 #_______________________________________________________________________________
 # Test severity functions ------------------------------------------------------
 #_______________________________________________________________________________
 
+test_that("pipgd_pov_severity_nv() - `params` works", {
+  params <- pipgd_params(
+    welfare = welfare_cumshare,
+    weight = weight_cumshare
+  )
+  ps_params <- pipgd_pov_severity_nv(
+    params = params
+  )
+  ps <- pipgd_pov_severity_nv(
+    welfare = welfare_cumshare,
+    weight  = weight_cumshare
+  )
+
+  expect_equal(
+    ps, ps_params
+  )
+
+})
+test_that("pipgd_pov_severity_nv() - `mean` works", {
+
+  ps1 <- pipgd_pov_severity_nv(
+    welfare = welfare_cumshare,
+    weight  = weight_cumshare,
+    mean    = 1
+  )
+  psnone <- pipgd_pov_severity_nv(
+    welfare = welfare_cumshare,
+    weight  = weight_cumshare
+  )
+  psnull <- pipgd_pov_severity_nv(
+    welfare = welfare_cumshare,
+    weight  = weight_cumshare,
+    mean    = NULL
+  )
+
+  expect_equal(
+    ps1, psnone
+  )
+  expect_equal(
+    ps1, psnull
+  )
+})
+
+
+
+test_that("pipgd_pov_severity_nv() - `lorenz` works", {
+
+})
+test_that("pipgd_pov_severity_nv() - `pov_gap` works", {
+
+})
+test_that("pipgd_pov_severity_nv() - `complete` works", {
+
+})
 
 
 
