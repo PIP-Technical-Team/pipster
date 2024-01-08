da8736987471167aaf10234d21f142419f3ad5db
zander-prinsloo <prinsloo.zander@gmail.com>
2024-01-08 10:30:43 -0500

    add watts index md

--- R/pipmd_pov.R
+++ R/pipmd_pov.R
@@ -271,7 +271,7 @@ pipmd_pov_gap <- function(
 #'
 #' @inheritParams pipmd_pov_headcount
 #'
-#' @return numeric: Poverty gap
+#' @return numeric: Poverty severity
 #' @keywords internal
 pipmd_pov_severity_nv <- function(
     welfare    = NULL,
@@ -370,6 +370,130 @@ pipmd_pov_severity <- function(
   out <- return_format_md(
     ld      = list_pov_severity,
     var     = "pov_severity",
+    format  = format,
+    povline = povline
+  )
+
+  # ____________________________________________________________________________
+  # Return ---------------------------------------------------------------------
+  return(out)
+
+
+}
+
+
+
+
+# WATTS -----------------------------------------------------------------
+#-------------------------------------------------------------------------------
+
+
+#' Calculate Watts index using microdata
+#'
+#' Non-vectorized Watts index microdata function. Use the vectorized
+#' function [pipmd_watts]
+#'
+#' @inheritParams pipmd_pov_headcount
+#'
+#' @return numeric: Watts index
+#' @keywords internal
+pipmd_watts_nv <- function(
+    welfare    = NULL,
+    weight     = NULL,
+    povline    = ifelse(!is.na(mean*times_mean),
+                        mean*times_mean,
+                        NULL
+    ),
+    mean       = 1,
+    times_mean = 1
+){
+  # ____________________________________________________________________________
+  # Arguments ------------------------------------------------------------------
+  if (is.na(welfare) |> any()) {
+    cli::cli_abort("No elements in welfare vector can be NA")
+  }
+  if (length(weight) > 1 & any(is.na(weight))) {
+    cli::cli_abort("No elements in weight vector can be NULL")
+  }
+  if (is.null(weight)) {
+    weight <- rep(1, length = length(welfare))
+    cli::cli_alert_warning(
+      text = "No weight vector specified, each observation assigned equal weight"
+    )
+  }
+  if (is.null(povline) && is.numeric(povline)) {
+    cli::cli_abort(
+      text = "A numeric poverty line must be specified"
+    )
+  } else if (povline < min(welfare) && povline > max(welfare)) {
+    cli::cli_alert_info(
+      text = "Note: specified poverty line is not within the welfare range"
+    )
+  }
+
+  # ____________________________________________________________________________
+  # Computations ---------------------------------------------------------------
+  output <- list()
+  wi <- wbpip:::md_compute_poverty_stats(
+    welfare      = welfare,
+    weight       = weight,
+    povline_lcu  = povline
+  )
+  output$watts <- wi$watts
+
+  # ____________________________________________________________________________
+  # Return ---------------------------------------------------------------------
+  return(output)
+
+}
+
+
+#' Calculate Watts index from microdata
+#'
+#' @inheritParams pipmd_watts_nv
+#' @param format atomic character vector: specifies the format of output, either
+#' "dt", "list", or "atomic"
+#'
+#' @return list: contains numeric Watts index. See `format`
+#' @export
+#'
+#' @examples
+#' pipmd_pov_watts(
+#' welfare = pipmd_s$welfare,
+#' weight  = pipmd_s$weight,
+#' format  = "list"
+#' )
+pipmd_watts <- function(
+    welfare    = NULL,
+    weight     = NULL,
+    povline    = NULL,
+    mean       = 1,
+    times_mean = 1,
+    format     = c("dt", "list", "atomic")
+){
+
+  # ____________________________________________________________________________
+  # Arguments ------------------------------------------------------------------
+  format <- match.arg(format)
+
+  # ____________________________________________________________________________
+  # Computations ---------------------------------------------------------------
+  pipmd_watts_v <- Vectorize(
+    FUN            = pipmd_watts_nv,
+    vectorize.args = "povline",
+    SIMPLIFY       = FALSE
+  )
+  list_watts <- pipmd_watts_v(
+    welfare    = welfare,
+    weight     = weight,
+    povline    = povline
+  )
+
+  # ____________________________________________________________________________
+  # Format ---------------------------------------------------------------------
+  out <- return_format_md(
+    ld      = list_watts,
+    var     = "watts",
     format  = format,
     povline = povline
   )
