4fa6b4dbd667a406a97d2e800c1aff8ad982d592
zander-prinsloo <prinsloo.zander@gmail.com>
2024-01-08 10:23:41 -0500

    add poverty severity

--- R/pipmd_pov.R
+++ R/pipmd_pov.R
@@ -257,3 +257,130 @@ pipmd_pov_gap <- function(
 
 
 
+
+
+
+# POV SEVERITY -----------------------------------------------------------------
+#-------------------------------------------------------------------------------
+
+
+#' Calculate poverty severity using microdata
+#'
+#' Non-vectorized poverty severity microdata function. Use the vectorized
+#' function [pipmd_pov_severity]
+#'
+#' @inheritParams pipmd_pov_headcount
+#'
+#' @return numeric: Poverty gap
+#' @keywords internal
+pipmd_pov_severity_nv <- function(
+    welfare    = NULL,
+    weight     = NULL,
+    povline    = ifelse(!is.na(mean*times_mean),
+                        mean*times_mean,
+                        NULL
+    ),
+    mean       = 1,
+    times_mean = 1
+){
+  # ____________________________________________________________________________
+  # Arguments ------------------------------------------------------------------
+  if (is.na(welfare) |> any()) {
+    cli::cli_abort("No elements in welfare vector can be NA")
+  }
+  if (length(weight) > 1 & any(is.na(weight))) {
+    cli::cli_abort("No elements in weight vector can be NULL")
+  }
+  if (is.null(weight)) {
+    weight <- rep(1, length = length(welfare))
+    cli::cli_alert_warning(
+      text = "No weight vector specified, each observation assigned equal weight"
+    )
+  }
+  if (is.null(povline) && is.numeric(povline)) {
+    cli::cli_abort(
+      text = "A numeric poverty line must be specified"
+    )
+  } else if (povline < min(welfare) && povline > max(welfare)) {
+    cli::cli_alert_info(
+      text = "Note: specified poverty line is not within the welfare range"
+    )
+  }
+
+  # ____________________________________________________________________________
+  # Computations ---------------------------------------------------------------
+  output <- list()
+  pg <- wbpip:::md_compute_poverty_stats(
+    welfare      = welfare,
+    weight       = weight,
+    povline_lcu  = povline
+  )
+  output$pov_severity <- pg$poverty_severity
+
+  # ____________________________________________________________________________
+  # Return ---------------------------------------------------------------------
+  return(output)
+
+}
+
+
+#' Calculate poverty severity from microdata
+#'
+#' @inheritParams pipmd_pov_severity_nv
+#' @param format atomic character vector: specifies the format of output, either
+#' "dt", "list", or "atomic"
+#'
+#' @return list: contains numeric poverty severity. See `format`
+#' @export
+#'
+#' @examples
+#' pipmd_pov_severity(
+#' welfare = pipmd_s$welfare,
+#' weight  = pipmd_s$weight,
+#' format  = "list"
+#' )
+pipmd_pov_severity <- function(
+    welfare    = NULL,
+    weight     = NULL,
+    povline    = NULL,
+    mean       = 1,
+    times_mean = 1,
+    format     = c("dt", "list", "atomic")
+){
+
+  # ____________________________________________________________________________
+  # Arguments ------------------------------------------------------------------
+  format <- match.arg(format)
+
+  # ____________________________________________________________________________
+  # Computations ---------------------------------------------------------------
+  pipmd_pov_severity_v <- Vectorize(
+    FUN            = pipmd_pov_severity_nv,
+    vectorize.args = "povline",
+    SIMPLIFY       = FALSE
+  )
+  list_pov_severity <- pipmd_pov_severity_v(
+    welfare    = welfare,
+    weight     = weight,
+    povline    = povline
+  )
+
+  # ____________________________________________________________________________
+  # Format ---------------------------------------------------------------------
+  out <- return_format_md(
+    ld      = list_pov_severity,
+    var     = "pov_severity",
+    format  = format,
+    povline = povline
+  )
+
+  # ____________________________________________________________________________
+  # Return ---------------------------------------------------------------------
+  return(out)
+
+
+}
+
+
+
+
