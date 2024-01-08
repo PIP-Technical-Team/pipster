d3ce91fa99acd030694617bba44bd97c3e4b506f
zander-prinsloo <prinsloo.zander@gmail.com>
2024-01-08 10:16:39 -0500

    add md pov gap functions

--- R/pipmd_pov.R
+++ R/pipmd_pov.R
@@ -84,10 +84,12 @@ pipmd_pov_headcount_nv <- function(
 #' format  = "list"
 #' )
 pipmd_pov_headcount <- function(
-    welfare = NULL,
-    weight  = NULL,
-    povline = NULL,
-    format  = c("dt", "list", "atomic")
+    welfare    = NULL,
+    weight     = NULL,
+    povline    = NULL,
+    mean       = 1,
+    times_mean = 1,
+    format     = c("dt", "list", "atomic")
 ){
 
   # ____________________________________________________________________________
@@ -122,5 +124,136 @@ pipmd_pov_headcount <- function(
 
 
 }
+
+
+
+
+
+
+
+
+
+# POV GAP-----------------------------------------------------------------------
+#-------------------------------------------------------------------------------
+
+
+#' Calculate poverty gap using microdata
+#'
+#' Non-vectorized poverty gap microdata function. Use the vectorized
+#' function [pipmd_pov_gap]
+#'
+#' @inheritParams pipmd_pov_headcount
+#'
+#' @return numeric: Poverty gap
+#' @keywords internal
+pipmd_pov_gap_nv <- function(
+    welfare    = NULL,
+    weight     = NULL,
+    povline    = ifelse(!is.na(mean*times_mean),
+                        mean*times_mean,
+                        NULL
+    ),
+    mean       = 1,
+    times_mean = 1
+){
+  # ____________________________________________________________________________
+  # Arguments ------------------------------------------------------------------
+  if (is.na(welfare) |> any()) {
+    cli::cli_abort("No elements in welfare vector can be NA")
+  }
+  if (length(weight) > 1 & any(is.na(weight))) {
+    cli::cli_abort("No elements in weight vector can be NULL")
+  }
+  if (is.null(weight)) {
+    weight <- rep(1, length = length(welfare))
+    cli::cli_alert_warning(
+      text = "No weight vector specified, each observation assigned equal weight"
+    )
+  }
+  if (is.null(povline) && is.numeric(povline)) {
+    cli::cli_abort(
+      text = "A numeric poverty line must be specified"
+    )
+  } else if (povline < min(welfare) && povline > max(welfare)) {
+    cli::cli_alert_info(
+      text = "Note: specified poverty line is not within the welfare range"
+    )
+  }
+
+  # ____________________________________________________________________________
+  # Computations ---------------------------------------------------------------
+  output <- list()
+  pg <- wbpip:::md_compute_poverty_stats(
+    welfare      = welfare,
+    weight       = weight,
+    povline_lcu  = povline
+  )
+  output$pov_gap <- pg$poverty_gap
+
+  # ____________________________________________________________________________
+  # Return ---------------------------------------------------------------------
+  return(output)
+
+}
+
+
+#' Calculate poverty gap from microdata
+#'
+#' @inheritParams pipmd_pov_gap_nv
+#' @param format atomic character vector: specifies the format of output, either
+#' "dt", "list", or "atomic"
+#'
+#' @return list: contains numeric poverty gap See `format`
+#' @export
+#'
+#' @examples
+#' pipmd_pov_gap(
+#' welfare = pipmd_s$welfare,
+#' weight  = pipmd_s$weight,
+#' format  = "list"
+#' )
+pipmd_pov_gap <- function(
+    welfare    = NULL,
+    weight     = NULL,
+    povline    = NULL,
+    mean       = 1,
+    times_mean = 1,
+    format     = c("dt", "list", "atomic")
+){
+
+  # ____________________________________________________________________________
+  # Arguments ------------------------------------------------------------------
+  format <- match.arg(format)
+
+  # ____________________________________________________________________________
+  # Computations ---------------------------------------------------------------
+  pipmd_pov_gap_v <- Vectorize(
+    FUN            = pipmd_pov_gap_nv,
+    vectorize.args = "povline",
+    SIMPLIFY       = FALSE
+  )
+  list_pov_gap <- pipmd_pov_gap_v(
+    welfare    = welfare,
+    weight     = weight,
+    povline    = povline
+  )
+
+  # ____________________________________________________________________________
+  # Format ---------------------------------------------------------------------
+  out <- return_format_md(
+    ld      = list_pov_gap,
+    var     = "pov_gap",
+    format  = format,
+    povline = povline
+  )
+
+  # ____________________________________________________________________________
+  # Return ---------------------------------------------------------------------
+  return(out)
+
+
+}
+
+
 
 
