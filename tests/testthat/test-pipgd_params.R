522e4b8e97798c249a8eec8b74865e4a48a62fa1
R.Andres Castaneda Aguilar <acastanedaa@worldbank.org>
2024-01-12 18:56:29 -0500

    add some tests to pipgd_params

--- tests/testthat/test-pipgd_params.R
+++ tests/testthat/test-pipgd_params.R
@@ -1,9 +1,30 @@
-test_that("pipgd_params works", {
+
+test_that("pipgd_params output", {
   res <- pipgd_params(
     welfare = pip_gd$L,
     weight = pip_gd$P)
-
+  # Class --------
   class(res) |>
     expect_equal("pipgd_params")
+
+  # Names -------------
+  names(res) |>
+    expect_equal(c("gd_params", "data"))
+
+  names(res$gd_params) |>
+    expect_equal(c("lq", "lb"))
+
+
+  names(res$gd_params$lq) |>
+    expect_equal(names(res$gd_params$lb))
+
+
+  names(res$gd_params$lq) |>
+    expect_equal(c("reg_results", "key_values"))
+
+
+  names(res$data) |>
+    expect_equal(c("welfare", "weight"))
+
 
 })
