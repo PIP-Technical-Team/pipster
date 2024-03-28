b7d06142978efe1c9ae4d0efb6aca19a352ee49d
giorgiacek <giorgivola@gmail.com>
2024-03-28 16:28:54 -0400

    pov_gap() adjusted to take pip object

--- R/pipmd_pov.R
+++ R/pipmd_pov.R
@@ -182,30 +182,53 @@ pipmd_pov_gap_nv <- function(
     welfare        = NULL,
     weight     = rep(1, length = length(welfare)),
     povline    = fmean(welfare, w = weight)*times_mean,
-    times_mean = 1)
+    times_mean = NULL)
   {
-  #   Defenses -------------
-  if (!is.null(pipster_object)) {
-    welfare <- pipster_object$welfare |> unclass()
-    weight <- pipster_object$weight |> unclass()
-  }
-  check_pipmd_pov()
 
-  # ____________________________________________________________________________
+  # ----------------------------------------------------------------------------
+  # Arguments ------------------------------------------------------------------
+
+  pl <- as.list(environment())
+  po <- is_valid_inputs_md(pl)
+
+
+  # Adjust povline and times_mean giving pipster_object precedent
+  if (!po & is.null(welfare)) {
+    pipster_object <- create_pipster_object(welfare = pipster_object$welfare |> unclass(),
+                                            weight  = pipster_object$weight |> unclass(),
+                                            povline = povline)
+  }
+
+  if (!po & !is.null(welfare)) {
+    pipster_object <- create_pipster_object(welfare = welfare,
+                                            weight  = weight,
+                                            povline = povline)
+  }
+
+  check_pipmd_pov(pl)
+
+  # ----------------------------------------------------------------------------
   # Computations ---------------------------------------------------------------
-  output <- list()
-  pg <- wbpip::md_compute_fgt(
-    welfare      = welfare,
-    weight       = weight,
-    povline      = povline,
-    alpha        = 1
+
+  fgt_data <- wbpip::md_compute_fgt(
+    welfare      = pipster_object$welfare |> unclass(),
+    weight       = pipster_object$weight |> unclass(),
+    povline      = pipster_object$args$povline,
+    alpha        = 1,
+    return_data  = TRUE # this is to carry results to use in next calculations
   )
-  attributes(pg) <- NULL
-  output$pov_gap <- pg
+
 
   # ____________________________________________________________________________
   # Return ---------------------------------------------------------------------
-  output
+
+  results <- pipster_object$results
+  results$pov_stats$fgt_data <- fgt_data
+  results$pov_stats$pov_gap <- fgt_data$FGT1
+  results$pov_stats$povline <- fgt_data$povline
+
+
+  pipster_object$results <- results
 
 }
 
@@ -248,12 +271,18 @@ pipmd_pov_gap <- function(
     weight     = rep(1, length = length(welfare)),
     povline    = fmean(welfare, w = weight)*times_mean,
     times_mean = 1,
-    format     = c("dt", "list", "atomic")
+    format     = c("dt", "list", "atomic"),
+    complete = getOption('pipster.return_complete')
 ){
 
   # ____________________________________________________________________________
   # Arguments ------------------------------------------------------------------
   format <- match.arg(format)
+
+  ## povline check: user-defined povline has priority
+  if (is.null(povline) && !is.null(pipster_object$args$povline)) {
+    povline <- pipster_object$args$povline
+  }
 
   # ____________________________________________________________________________
   # Computations ---------------------------------------------------------------
@@ -262,6 +291,7 @@ pipmd_pov_gap <- function(
     vectorize.args = "povline",
     SIMPLIFY       = FALSE
   )
+
   list_pov_gap <- pipmd_pov_gap_v(
     pipster_object = pipster_object,
     welfare    = welfare,
@@ -271,11 +301,12 @@ pipmd_pov_gap <- function(
 
   # ____________________________________________________________________________
   # Format ---------------------------------------------------------------------
-  out <- return_format_md(
+  out <- return_format_md_pov(
     ld      = list_pov_gap,
     var     = "pov_gap",
     format  = format,
-    povline = povline
+    povline = povline,
+    complete = complete
   )
 
   # ____________________________________________________________________________
